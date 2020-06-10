2bb90b9154cceed10a965cc8adf80f534345b5ae
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2020-06-10 14:23:54 -0400

    Updated SwiftPatches to v2.0 Changed dependencies of CustomInts from SwiftPatches to NumericPatches

--- Package.swift
+++ Package.swift
@@ -15,13 +15,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/TheAngryDarling/SwiftCustomCoders.git", from: "1.0.2"),
+        .package(url: "https://github.com/TheAngryDarling/SwiftPatches.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CustomInts",
-            dependencies: []),
+            dependencies: ["NumericPatches"]),
         .testTarget(
             name: "CustomIntsTests",
             dependencies: ["CustomInts", "CustomCoders"]),
